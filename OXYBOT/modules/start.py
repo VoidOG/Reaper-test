abeda64312f241e33675e9b89662b5dec83c4f2f
VoidOG <VoidGod123@outlook.com>
2024-09-23 17:56:04 +0530

    Update start.py

--- OXYBOT/modules/start.py
+++ OXYBOT/modules/start.py
@@ -32,7 +32,7 @@ async def start(event):
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nᴡᴇʟᴄᴏᴍᴇ ᴛᴏ [{bot_name}](tg://user?id={bot_id}) , ᴛʜᴇ ᴜʟᴛɪᴍᴀᴛᴇ ᴡᴇᴀᴘᴏɴ ꜰᴏʀ ᴄʜᴀᴏs ᴏɴ ᴛᴇʟᴇɢʀᴀᴍ! ᴀs ᴛʜᴇ ᴍᴏsᴛ ᴘᴏᴡᴇʀꜰᴜʟ sᴘᴀᴍ ʙᴏᴛ ᴀᴠᴀɪʟᴀʙʟᴇ, ʀᴇᴀᴘᴇʀ ᴄᴀɴ ᴜɴʟᴇᴀsʜ ʀᴇʟᴇɴᴛʟᴇss ᴡᴀᴠᴇs ᴏf ᴍᴇssᴀɢᴇs ᴀɴᴅ ʟᴀᴜɴᴄʜ ʀᴀɪᴅs ᴏɴ ᴜsᴇʀs ᴡɪᴛʜ ᴘʀᴇᴄɪsɪᴏɴ. ᴅᴇsɪɢɴᴇᴅ ꜰᴏʀ ᴛʜᴏsᴇ ᴡʜᴏ ᴅᴀʀᴇ ᴛᴏ ᴘᴜsʜ ʙᴏᴜɴᴅᴀʀɪᴇs, ᴛʜɪs ʙᴏᴛ ᴏғғᴇʀs ᴜɴᴘᴀʀᴀʟʟᴇʟᴇᴅ ᴄᴀᴘᴀʙɪʟɪᴛɪᴇs ꜰᴏʀ sᴘᴀᴍᴍɪɴɢ ᴀɴᴅ ᴜsᴇʀ ᴇɴɢᴀɢᴇᴍᴇɴᴛ. ᴇᴍʙʀᴀᴄᴇ ᴛʜᴇ ᴘᴏᴡᴇʀ ᴏf ʀᴇᴀᴘᴇʀ ᴀɴᴅ ᴛᴀᴋᴇ ᴄᴏɴᴛʀᴏʟ ᴏf ᴛʜᴇ ᴅɪɢɪᴛᴀʟ ʙᴀᴛᴛʟᴇfɪᴇʟᴅ!**\n"
         await event.client.send_file(
             event.chat_id,
-            "https://telegra.ph/file/b11e7d86e4622a3b3e54e.jpg",
+            "https://i.imghippo.com/files/Bq5B71727094310.jpg",
             caption=TEXT,
             buttons=START_BUTTON
         )
